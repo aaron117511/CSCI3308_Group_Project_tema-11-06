2b41b1879ff0b843753fe8a063363c966cfdaa9d
aaron117511 <aacu0007@colorado.edu>
2023-04-23 00:29:19 -0600

    fixing git issues with rebasing

--- src/index.js
+++ src/index.js
@@ -228,23 +228,56 @@ app.get('/authentication', async (req, res) => {
   }
 });
 
+function RefreshToken(){
+  const xhr = new XMLHttpRequest();
 
-
-
-// To check status use this.status
-        // to access data use datd.<element id>
-function reqListener() {
-  console.log(this.responseText);
+  xhr.open("POST", "https://accounts.spotify.com/api/token", true);              // create api call
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');     // add a header to the api call
+  xhr.setRequestHeader('Authorization', 'Basic ' + btoa(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET));   
+  xhr.send("grant_type=refresh_token" + "&refresh_token=" + session.refresh_token + "&client_id=" + process.env.CLIENT_ID); // send the api call
+    // once we get the request back from spotify update the access_token
+  xhr.onload = () =>{
+          var data = JSON.parse(this.responseText);
+          /* i think the following should update the data base but im unsure
+          var update_query = `UPDATE users SET access_token = $1 WHERE username = $3 RETURNING *;`;          
+          db.any(update_query, [
+                                data.access_token,
+                                session.user.username
+                              ]);
+          */
+        };
 }
+
+
+function requestData(endpoint, callType, body) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(callType, endpoint);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + session.user.access_token);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.responseType = 'json'
+  xhr.onload = callApi(endpoint, callType, body) 
+  if(body) xhr.send(JSON.stringify(body));
+  else xhr.send();
+}
+
+
+// call this function when you want to make an api call
+//    endpoint is the url given to you by spotify
+//    callType is the type of api call you're making as a string ie: <"POST">
+//            note: must be in all capps!
+//    body is the extra parameters that spotify asks you to include
+//            note: this doesnt have to be used and can be passed entirley in the enpoint varible 
+//                  this is more of here for ease of use and flexability while you imlementing api calls to spotify
+//            note: if you dont use this at all please pass through null instead
 function callApi(endpoint, callType, body){
-  const req = new XMLHttpRequest();
-  req.onload = () => {
-    console.log(req.responseXML);
+  requestData(endpoint, callType, body);
+  if(this.status == 200){
+  return xhr.responseXML;
   }
-  req.addEventListener("GET", reqListener);
-  req.setRequestHeader('Authorization', 'Bearer ' + req.session.user.refresh_token);
-  req.open(callType, endpoint);
-  req.send(body);
+  if (this.status == 401) {
+    RefreshToken();
+    requestData(endpoint, callType, body);
+  }
 }
 
         
